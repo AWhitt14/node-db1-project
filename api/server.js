940fe0bbe1fa78c3f0808c365fd11fffc32441c4
Alex Whitt <whitta14@gmail.com>
2020-10-14 21:32:56 -0400

    completed assignment

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const budgetRouter = require('../budget-router');
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(budgetRouter);
 
+
+server.use((er, req, res, next) => {
+    console.log(er);
+    res.status(500).json({message: 'server side error'})
+})
 module.exports = server;
